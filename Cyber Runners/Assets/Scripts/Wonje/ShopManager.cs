f203b9f215c23e3a2322edba7551030968c2779a
reductant01 <hwj07280728@gmail.com>
2025-02-08 03:44:55 +0900

    씬전환

--- Cyber Runners/Assets/Scripts/Wonje/ShopManager.cs
+++ Cyber Runners/Assets/Scripts/Wonje/ShopManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ShopManager : MonoBehaviour
 {
@@ -16,8 +17,13 @@ public class ShopManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void ToShop()
     {
-        
+        SceneManager.LoadScene("Wonje");
+    }
+
+    public void ToGame()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 }
